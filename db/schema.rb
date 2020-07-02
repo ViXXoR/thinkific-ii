d7ddf889c0f37f7f33c94a6d5443b0c198380bd6
Matthew Livingstone <mattliving@vianet.ca>
2020-07-02 17:16:27 -0400

    Add schema support and gems for omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_181406) do
+ActiveRecord::Schema.define(version: 2020_07_02_185836) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_06_30_181406) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
