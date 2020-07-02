cbe9db63f15072896bf492cbb4dfe4bd934a2a9c
ViXXoR <iamvixxor@gmail.com>
2020-07-02 16:48:46 -0400

    Add hosts for running on VPS

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module IncrementingApi
 
     config.autoload_paths << Rails.root.join("lib")
 
+    config.hosts << "mmrlivingstone.com"
+    config.hosts << "cloud.mmrlivingstone.com"
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
